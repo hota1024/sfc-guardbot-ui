8e3387a604bffcc2b81b382b572f6bc794ae4962
= <hotalog@hota1024.com>
2022-03-22 16:42:14 +0900

    🔨add theme and media config

--- src/stiches.config.ts
+++ src/stiches.config.ts
@@ -9,4 +9,18 @@ export const {
   styled,
   theme,
   keyframes,
-} = createStitches()
+} = createStitches({
+  theme: {
+    colors: {
+      yellow: '#FDD000',
+      blue: '#5865F2',
+      black: '#23272A',
+    },
+  },
+  media: {
+    xs: '(max-width: 599px)',
+    sm: '(min-width: 600px)',
+    md: '(min-width: 1200px)',
+    lg: '(min-width: 1800px)',
+  },
+})
