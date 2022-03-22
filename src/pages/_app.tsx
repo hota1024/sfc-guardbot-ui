2965626b35f8784e84b2a031e1e317552e830a3a
= <hotalog@hota1024.com>
2022-03-22 16:43:15 +0900

    💄add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import { UserProvider } from '@auth0/nextjs-auth0'
+import { globalCss } from '@stitches/react'
 import type { AppProps } from 'next/app'
 
+const globalStyles = globalCss({
+  '*': { margin: 0, padding: 0, boxSizing: 'border-box' },
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
+
   return (
     <>
       <UserProvider>
